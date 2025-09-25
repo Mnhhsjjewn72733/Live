c8c5061ec45d6c088ca1143bf857fec6f88440e9
Sundarraj G <148694616+Mnhhsjjewn72733@users.noreply.github.com>
2025-09-25 11:48:42 +0530

    Update script.js

--- Products/script.js
+++ Products/script.js
@@ -5,7 +5,7 @@ const products = [
         category: "protein",
         price: 2500,
         images: [
-            "Images/download__5_-removebg-preview.png",
+            "./Products/Images/download__5_-removebg-preview.png",
             "https://images.unsplash.com/photo-1594736797933-d0401ba2fe65?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80"
         ],
         variants: [
@@ -496,4 +496,5 @@ window.addEventListener('scroll', () => {
         navbar.style.backdropFilter = 'blur(10px)';
     }
 });
+
 
