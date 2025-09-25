347eff51bb8a7575019fe9f74d240c0179f964ae
Sundarraj G <148694616+Mnhhsjjewn72733@users.noreply.github.com>
2025-09-25 11:51:10 +0530

    Update script.js

--- Products/script.js
+++ Products/script.js
@@ -5,7 +5,7 @@ const products = [
         category: "protein",
         price: 2500,
         images: [
-            "./Products/Images/download__5_-removebg-preview.png",
+            "Products/Images/download__5_-removebg-preview.png",
             "https://images.unsplash.com/photo-1594736797933-d0401ba2fe65?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80"
         ],
         variants: [
@@ -21,7 +21,7 @@ const products = [
         category: "protein",
         price: 2800,
         images: [
-            "/Products/Images/images-removebg-preview.png",
+            "Products/Images/images-removebg-preview.png",
             "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80"
         ],
         variants: [
@@ -37,7 +37,7 @@ const products = [
         category: "accessories",
         price: 800,
         images: [
-            "/Products/Images/adjustable_dumbells.png",
+            "Products/Images/adjustable_dumbells.png",
             "https://images.unsplash.com/photo-1594737625785-a6cbdabd333c?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80"
         ],
         variants: [
@@ -496,5 +496,6 @@ window.addEventListener('scroll', () => {
         navbar.style.backdropFilter = 'blur(10px)';
     }
 });
+
 
 
